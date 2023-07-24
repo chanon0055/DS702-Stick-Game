48c1944b2e4eb1eda3a408742384e7f72a39774b
CHANON HOMEPIROM <chanon_h@cmu.ac.th>
2023-07-25 00:13:18 +0700

    rev.2
    if player win code don't stop run

--- HW1E_660631096.py
+++ HW1E_660631096.py
@@ -18,22 +18,21 @@ def setup_stp_pname():
 # Crate Function condition and calculation.(Human Player)
 def player_turn():
     global st_take, st_all
-    while st_all > 0 :
-        st_take = int(input(user_name + ",how many sticks you will take (1 or 2):"))
-        if st_take >= 3 :
+    st_take = int(input(user_name + ",how many sticks you will take (1 or 2):"))
+    if st_take >= 3 :
             print("No you cannot take more than 2 sticks!")
             print()
-        elif st_take <= 0 :
+    elif st_take <= 0 :
             print("No you cannot take less than 1 skick!")
             print()
-        elif (st_all - st_take) < 0 :
+    elif (st_all - st_take) < 0 :
             print("Threr are no enough sticks to take.")
             print("\n")
-        elif (st_all - st_take) > 0 :
+    elif (st_all - st_take) > 0 :
             st_all = st_all - st_take
             print("There are ",st_all," sticks in the pile.")
             print("\n")
-        else :
+    else :
             st_all = st_all - st_take
             print(user_name + ", take the last stick")
             print("\n")
@@ -64,23 +63,33 @@ def com_random_mode_turn():
             
 # Crate Function condition and calculation.(AI Mode)
 def com_ai_mode_turn():
-    global random_number, st_all 
+    global st_all, magic_number, check_magic_num, st_ai_take
     random_number = random.randint(1, 2)
-    magic_number = 
-    if st_take >= 3 :
-        print()
-    elif st_take <= 0 :
-        print()
-    elif st_all > 0 :
-        st_pc_take = random_number
-        while st_pc_take > st_all :
-           st_pc_take = random_number 
-        if (st_all - st_pc_take) > 0 :
-               print("I, smart computer, take : " + str(st_pc_take))
-               st_all = st_all - st_pc_take
-               print("There are " + str(st_all) + " sticks in the pile.")
-               print("\n")
-        else :
-            st_all = st_all - st_pc_take
-            print("I, smart computer, takes the last stick.\n")
-            print(user_name + " win (I, smart computer, am sad T_T)")
+    magic_number = (st_all-1)%3
+    if magic_number != 0 :
+            st_ai_take = 2
+            check_magic_num = ((st_all-st_ai_take)-1)%3
+            if check_magic_num != 0:
+                st_ai_take = 1
+            else:
+                st_ai_take = 2
+            st_all = st_all - st_ai_take
+            print("I take " + str(st_ai_take) +" sticks There are " + str(st_all) + " sticks in the pile.")
+            print("\n")
+    elif magic_number == 0 and st_all > 1:
+            st_ai_take = random_number
+            st_all = st_all - st_ai_take
+            print("I take " + str(st_ai_take) +" sticks There are " + str(st_all) + " sticks in the pile.")
+            print("\n")
+    else :
+            st_all = st_all - st_ai_take
+            print("AI, takes the last stick.")
+            print(user_name + " win (AI sad T_T)")
+            
+
+setup_stp_pname()
+while st_all > 0:
+    com_ai_mode_turn()
+    player_turn()
+
+    
